257b465f359ec8f0e93f4f3a1df7ff2ab9ce8511
shaihitdin@gmail.com <shaihitdin@gmail.com>
2017-05-20 09:38:41 +0600

    20

--- Task1/Task1/Program.cs
+++ Task1/Task1/Program.cs
@@ -9,34 +9,53 @@ namespace Task1
 {
     class Program
     {
-        static int ReadInt (StreamReader a)
+        static int[] ReadInt (StreamReader a)
         {
-            int x = 0;
-            do
+            while (true)
             {
-                char c = (char)a.Read();
-                if (c == null)
-                    break;
-                x = x * 10 + (c - '0');
-            } while (c != ' ' || c != '\n')
-            return x;
+                int[] tmp = a.ReadLine().Split(' ');
+            }
         }
+
+
 
         static void Main(string[] args)
         {
-            DirectoryInfo d = new DirectoryInfo(@"C:\Users\shaih_000\Documents\GitHub\final\Task1\bin\");
+            DirectoryInfo d = new DirectoryInfo(@"C:\Users\shaih_000\Documents\GitHub\final\Task1\Task1\bin\Debug\");
             foreach (FileInfo a in d.GetFiles())
             {
-                if (a.Name.Contains(".txt"))
+                if (a.Name.Length >= 4 && a.Name.Substring(a.Name.Length - 4) == ".txt")
                 {
                     StreamReader b = new StreamReader(new FileStream(a.Name, FileMode.Open, FileAccess.ReadWrite));
                     Console.WriteLine(a.Name + ": ");
                     int[] c = new int[2];
                     c[0] = ReadInt(b);
                     c[1] = ReadInt(b);
-                    for (int i = 2; i * i <= c; ++i)
+                    Console.WriteLine(c[0]);
+                    Console.WriteLine(c[1]);
+                    if (c[0] > c[1])
                     {
-                        if (c % i == 0 && d %)
+                        int tmp = c[0];
+                        c[0] = c[1];
+                        c[1] = tmp;
+                    }
+                    bool ok = false;
+                    for (int i = 2; i * i <= c[0]; ++i)
+                    {
+                        if (c[0] % i == 0 && c[1] % i == 0)
+                        {
+                            ok = true;
+                        }
+                    }
+                    if (c[1] % c[0] == 0)
+                        ok = true;
+                    if (ok)
+                    {
+                        Console.WriteLine("yes");
+                    }
+                    else
+                    {
+                        Console.WriteLine("no");
                     }
                 }
             }
