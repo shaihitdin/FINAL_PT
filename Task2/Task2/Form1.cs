6da4209e95df9b0b6d326d49d19af2efd17886f4
shaihitdin@gmail.com <shaihitdin@gmail.com>
2017-05-20 10:11:31 +0600

    10:10

--- Task2/Task2/Form1.cs
+++ Task2/Task2/Form1.cs
@@ -15,6 +15,25 @@ namespace Task2
         public Form1()
         {
             InitializeComponent();
+            Button[] b = new Button[9];
+            int[] clicks = new int[9];
+            for (int i = 0; i < 9; ++i)
+                clicks[i] = 0;
+            for (int i = 0; i < 3; ++i)
+            {
+                for (int j = 0; j < 3; ++j)
+                {
+                    b[i * 3 + j] = new Button();
+                    b[i * 3 + j].Location = new Point(50 + 40 * j, 50 + 40 * i);
+                    b[i * 3 + j].Size = new Size(30, 30);
+                    b[i * 3 + j].Name = (i * 3 + j).ToString();
+                    b[i * 3 + j].UseVisualStyleBackColor = true;
+                }
+            }
+            ResumeLayout(false);
+            PerformLayout();
+
         }
+
     }
 }
