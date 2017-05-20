2e4d96fa8b0e4e8cd6bf9f2745d00493f04f42d7
shaihitdin@gmail.com <shaihitdin@gmail.com>
2017-05-20 10:51:29 +0600

    10:50

--- Task2/Task2/Form1.cs
+++ Task2/Task2/Form1.cs
@@ -13,11 +13,11 @@ namespace Task2
     public partial class Form1 : Form
     {
         static Button[] b = new Button[9];
+        static int[] clicks = new int[9];
 
         public Form1()
         {
             InitializeComponent();
-            int[] clicks = new int[9];
             for (int i = 0; i < 9; ++i)
                 clicks[i] = 0;
             for (int i = 0; i < 3; ++i)
@@ -26,10 +26,11 @@ namespace Task2
                 {
                     b[i * 3 + j] = new Button();
                     b[i * 3 + j].Location = new Point(50 + 40 * j, 50 + 40 * i);
-                    b[i * 3 + j].Size = new Size(30, 30);
-                    b[i * 3 + j].Name = "Button_" + (i * 3 + j).ToString();
+                    b[i * 3 + j].Size = new Size(35, 35);
+                    b[i * 3 + j].Name = (i * 3 + j).ToString();
                     b[i * 3 + j].Text = "0";
                     b[i * 3 + j].UseVisualStyleBackColor = true;
+                    b[i * 3 + j].Click += new System.EventHandler(new System.EventHandler(this.button1_Click));
                     Controls.Add(b[i * 3 + j]);
                 }
             }
@@ -37,6 +38,21 @@ namespace Task2
 
         private void button1_Click(object sender, EventArgs e)
         {
+            for (int i = 0; i < 9; ++i)
+            {
+                if (sender == b[i])
+                {
+                    ++clicks[i];
+                    if (clicks[i] % 2 == 0)
+                    {
+                        int now = Convert.ToInt32(textBox1.Text);
+                        now++;
+                        textBox1.Text = now.ToString();
+                    }
+                    b[i].Text = clicks[i].ToString();
+                }
+            }
+
         }
     }
 }
